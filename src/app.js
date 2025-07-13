7d8d682453c9d7a756ddf0288c356a48fddade01
Kumar <kumarnaidukarri2002@gmail.com>
2025-07-13 12:35:18 +0530

    2 - exploring get post delete http methods, use()

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = new express();
 
-app.use("/home", (req, res) => {
-  res.send("home from server");
+// "get()" handles only GET requests.
+app.get("/user", (req, res) => {
+  res.send("Kumar");
 });
+app.post("/user", (req, res) => {
+  res.send("data saved into DB");
+});
+app.delete("/user", (req, res) => {
+  res.send("data deleted from DB");
+});
+
+// "use" handles all HTTP methods requests.
 app.use("/test", (req, res) => {
   res.send("test from server");
 });
