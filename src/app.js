45c1d9b8b853aa68cd699ea1477eb4395e8aad0b
Kumar <kumarnaidukarri2002@gmail.com>
2025-07-13 22:41:54 +0530

    4 - exploring Middlewares and RequestHandlers/ResponseHandlers/RouteHandlers functions

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
 
-//QueryParameters APIcall - http://localhost:3000/user?userId=6&password=testing
-//OP = {userId:'6',password:'testing'}
-app.get("/user", (req, res) => {
-  console.log(req.query); //obj
-});
-
-//RouteParameters APIcall - http://localhost:3000/home/8/Kumar
-//OP = {ID:'8',NAME:'Kumar'}
-app.get("/home/:ID/:NAME", (req, res) => {
-  console.log(req.params); //obj
-});
+// The actual function which sends the "Response" back to client during API call is called 'Request Handler Function' or 'Route Handler'.
+/* a middleware is a function that runs before the final route handler. mostly used for logging, Authentication, Validation.
+  'next' parameter and next() to call next middleware function*/
+// we can send only 'ONE RESPONSE' for a single API call.
+app.get(
+  "/home",
+  (req, res, next) => {
+    console.log("Middleware 1");
+    next(); //call next middleware function
+  },
+  (req, res, next) => {
+    console.log("Middleware 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Response Handler or Route Handler or Request Handler");
+    res.send("Response 3");
+  }
+);
 
 app.listen(3000, function () {
   console.log("Server listening on port 3000 ...");
