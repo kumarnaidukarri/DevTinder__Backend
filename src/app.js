b44e87de3a9947c36a075b45e49044b279a18f83
Kumar <kumarnaidukarri2002@gmail.com>
2025-07-17 20:36:10 +0530

    7 - basic Connection DB using mongoose package

--- src/app.js
+++ src/app.js
@@ -1,25 +1,15 @@
 const express = require("express");
+const { connectDB } = require("./config/database.js");
 const app = express();
 
-// 1.TRY-CATCH Error Handling
-app.get("/userData", (req, res) => {
-  try {
-    //DB logic
-    res.send("Data sent");
-  } catch (err) {
-    res.send("some error occurred");
-  }
-});
-
-// 2.Global Error Handling Middleware
-// it must be kept at the end, after all routes.
-app.use((err, req, res, next) => {
-  if (err) {
-    console.log("Error occurred"); //log for developer
-    res.status(500).send("Something went wrong");
-  }
-});
-
-app.listen(3000, function () {
-  console.log("Server listening on port 3000 ...");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection Success...");
+    //only after DB connection, listening for API client requests
+    app.listen(3000, function () {
+      console.log("Server listening on port 3000 ...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection Failed...");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://kumar:12345@cluster0.qaea0ag.mongodb.net/devTinder"
+  );
+};
+module.exports = { connectDB };
+
+/*
+//  we should connect database first and then, only we start "listening" for API client requests.
+//  because, before connection we can't serve clients who need DB related operations.
+connectDB
+  .then(() => {
+    console.log("Database connection Success...");
+  })
+  .catch((err) => {
+    console.error("Database connection Failed...");
+  });
+*/
