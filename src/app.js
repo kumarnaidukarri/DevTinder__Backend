4f095406752925dc85389958b6778951bb111e82
Kumar <kumarnaidukarri2002@gmail.com>
2025-07-20 21:21:35 +0530

    7.3 - Accept Client data and insert into DB & Signup API

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const { connectDB } = require("./config/database.js");
 const app = express();
 
+app.use(express.json()); // middleware converts JSON data into Javascript object
 const { userModel } = require("./models/user.js");
+
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the User model
-  const userObj = {
-    firstName: "kumar",
-    lastName: "karri",
-    email: "kumar@123.com",
-    password: "12345",
-  };
-  let userIns = userModel(userObj);
-
-  await userIns.save(); //data will save into Database
-  res.send("User added Successfully ...");
+  try {
+    // req.body = {firstName:"ram",lastName:"kumar",email:"ram@123.com",password:"abcde"}
+    let userIns = userModel(req.body);
+    await userIns.save(); //data will save into Database
+    res.send("User added Successfully ...");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
 });
 
 connectDB()
