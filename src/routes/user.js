cdb208c5cc80188efb7072c7e19e9f11268f0c23
Kumar <kumarnaidukarri2002@gmail.com>
2025-08-01 19:23:50 +0530

    9.8 - Updated User Router with APIs like get all connections, get Feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,28 +3,103 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth.js");
 const connectionRequestModel = require("../models/connectionRequest.js");
+const userModel = require("../models/user.js");
+
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "age",
+  "gender",
+  "photoUrl",
+  "about",
+  "skills",
+];
 
 // GET API - get all the Pending connection requests for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
-    const connectionRequest = await connectionRequestModel
+    const connectionRequests = await connectionRequestModel
       .find({
         toUserId: loggedInUser._id,
         status: "interested",
       })
-      .populate("fromUserId", [
-        "firstName",
-        "lastName",
-        "age",
-        "gender",
-        "photoUrl",
-        "about",
-        "skills",
-      ]); // Query with 'populate()'
+      .populate("fromUserId", USER_SAFE_DATA); // Query with 'populate()'
 
-    res.json({ message: "Data fetched successfully", data: connectionRequest });
+    res.json({
+      message: "Data fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+// GET API - get all my connections
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // others accepted my Requests, i accepted other Requests.
+    const connectionRequests = await connectionRequestModel
+      .find({
+        $or: [
+          { toUserId: loggedInUser._id, status: "accepted" },
+          { fromUserId: loggedInUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      } else {
+        return row.fromUserId;
+      }
+    });
+    res.json({ data: data });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+// Feed API
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    /* User should see all user profile cards except:
+     0. his own card
+     1. his connections
+     2. ignored people
+     3. already sent the connection request
+    */
+    const loggedInUser = req.user;
+
+    // Find all connection requests (sent + recieved)
+    const connectionRequests = await connectionRequestModel
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromUserId, toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await userModel
+      .find({
+        $and: [
+          { _id: { $nin: Array.from(hideUsersFromFeed) } },
+          { _id: { $ne: loggedInUser._id } },
+        ],
+      })
+      .select(USER_SAFE_DATA);
+    // 'select([field1,field2,...])' means it selects only 'mentioned Fields' from Documents.
+
+    res.send(connectionRequests);
   } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
